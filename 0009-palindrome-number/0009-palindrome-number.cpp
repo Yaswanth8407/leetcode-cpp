58f67ee2462d0c4a21c26587223085be5468621a
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-07-07 16:57:06 +0530

    Time: 1 ms (57.95%), Space: 8.5 MB (92.61%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int num = x;
+        long rev = 0;
+
+        while(num!=0){
+            rev = (rev * 10) + (num % 10);
+            num = num/10;
+        }
+
+        return x==rev;
+    }
+};
