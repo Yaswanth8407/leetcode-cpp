4ac5c7e170b5ad5b27d144e99963503882ccc36e
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-07-27 23:00:25 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,23 +3,20 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
 
-        // Find the first zero
-        int i = 0;
+        int i = 0;   // index, not value
+
         while (i < n && nums[i] != 0) {
             i++;
         }
 
-        // If there are no zeros, we're done
         if (i == n) return;
 
-        // j searches for the next non-zero
         int j = i + 1;
 
         while (j < n) {
             if (nums[j] != 0) {
                 swap(nums[i], nums[j]);
 
-                // Move i to the next zero
                 i++;
                 while (i < n && nums[i] != 0) {
                     i++;
