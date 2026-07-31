c5b9e57de06345742c29e9e1034a4e344e6b7c8c
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-07-31 22:41:21 +0530

    Time: 0 ms (100%), Space: 27.2 MB (53.62%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,9 +4,7 @@ public:
         int left = 0;
         int right = s.size()-1;
         while(left<right){
-            s[left] = s[left]^s[right];
-            s[right] = s[left]^s[right];
-            s[left] = s[left]^s[right];
+            swap(s[left],s[right]);
             left++;
             right--;
         }
